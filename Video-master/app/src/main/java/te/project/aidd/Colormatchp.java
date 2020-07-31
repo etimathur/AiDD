7e9404a50ba4530f17041190d3d7483b49de9938
swara07 <swarangidali@gmail.com>
2020-07-31 13:46:19 +0530

    percentage circle

--- Video-master/app/src/main/java/te/project/aidd/Colormatchp.java
+++ Video-master/app/src/main/java/te/project/aidd/Colormatchp.java
@@ -4,28 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class Colormatchp extends AppCompatActivity {
+    ProgressBar pbparent;
+    ProgressBar pbdoc;
+    TextView tparent,tdoc;
+    int i=0,j=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colormatchp);
+        pbparent=(ProgressBar) findViewById(R.id.progresss);
+        tparent=(TextView) findViewById(R.id.parenttext);
+        pbdoc=(ProgressBar) findViewById(R.id.docpro);
+        tdoc=(TextView) findViewById(R.id.doctext);
         GraphView graph = (GraphView) findViewById(R.id.graph2);
-        // graph.getGridLabelRenderer().setVerticalAxisTitle("Match Value");
-        graph.getGridLabelRenderer().setHorizontalAxisTitle("GAMES");
-        // graph.getViewport().setMinX(0);
-        //graph.getViewport().setMaxX(5);
+        graph.getGridLabelRenderer().setHorizontalAxisTitle("NO OF GAMES");
         graph.getViewport().setMinY(10);
         graph.getViewport().setMaxY(50);
 
         graph.getViewport().setYAxisBoundsManual(true);
-        // graph.getViewport().setXAxisBoundsManual(true);
-
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
                 new DataPoint(1, 45),
 
@@ -36,7 +43,10 @@ public class Colormatchp extends AppCompatActivity {
         });
         graph.addSeries(series);
         graph.setBackgroundColor(Color.WHITE);
-        series.setThickness(5);
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+        series.setTitle("SCORE");
+        series.setThickness(7);
         series.setColor(Color.BLUE);
         series.setAnimated(true);
         series.setDrawDataPoints(true);
@@ -45,7 +55,7 @@ public class Colormatchp extends AppCompatActivity {
 
         GraphView graph1 = (GraphView) findViewById(R.id.graph);
         //graph1.getGridLabelRenderer().setVerticalAxisTitle("Speed progress");
-        graph1.getGridLabelRenderer().setHorizontalAxisTitle("GAMES");
+        graph1.getGridLabelRenderer().setHorizontalAxisTitle(" NO OF GAMES");
 
         graph1.getViewport().setMinY(0);
         graph1.getViewport().setMaxY(100);
@@ -57,25 +67,31 @@ public class Colormatchp extends AppCompatActivity {
                 new DataPoint(2, 20),
                 new DataPoint(3, 55),
                 new DataPoint(4, 75),
-                new DataPoint(5, 90),
-//                new DataPoint(0, 70),
-//                new DataPoint(0, 80),
-//                new DataPoint(0, 60),
-//                new DataPoint(0, 30)
+                new DataPoint(5, 60),
 
         });
 
         graph1.addSeries(series1);
         graph1.setBackgroundColor(Color.WHITE);
         graph1.getViewport().setScalableY(true);
-        series1.setThickness(5);
+        graph1.getLegendRenderer().setVisible(true);
+        graph1.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+        series1.setTitle("TIME");
+        series1.setThickness(7);
         series1.setColor(Color.RED);
-        //series1.setDrawBackground(true);
         series1.setAnimated(true);
         series1.setDrawDataPoints(true);
         series1.setDataPointsRadius(10);
-        //series1.setBackgroundColor(Color.CYAN);
-        //series1.setDrawBackground(true);
+
+        pbparent.setMax(100);
+        pbparent.setProgress(60);
+        tparent.setText(60+"%");
+
+        pbdoc.setMax(100);
+        pbdoc.setProgress(75);
+        tdoc.setText(75+"%");
+
+
 
 
     }
--- Video-master/app/src/main/java/te/project/aidd/Flipcardsp.java
+++ Video-master/app/src/main/java/te/project/aidd/Flipcardsp.java
@@ -4,17 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class Flipcardsp extends AppCompatActivity {
+    ProgressBar pb,pb1;
+    private int i=0,j=0;
+    private TextView textView,textView1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flipcardsp);
+        pb=(ProgressBar) findViewById(R.id.progress1);
+        textView=(TextView) findViewById(R.id.tst);
+        textView1=(TextView) findViewById(R.id.ttxt);
+        pb1=(ProgressBar) findViewById(R.id.progress2);
+
         LineGraphSeries<DataPoint> series3 = new LineGraphSeries<DataPoint>(new DataPoint[]{
                 new DataPoint(1, 45),
 
@@ -28,11 +41,11 @@ public class Flipcardsp extends AppCompatActivity {
 
         GraphView graphA = (GraphView) findViewById(R.id.graphmp);
 // graph.getGridLabelRenderer().setVerticalAxisTitle("Match Value");
-        graphA.getGridLabelRenderer().setHorizontalAxisTitle("GAMES");
-
+        graphA.getGridLabelRenderer().setHorizontalAxisTitle(" NO OF GAMES");
+        graphA.getLegendRenderer().setVisible(true);
+       // graphA.getGridLabelRenderer().setVerticalAxisTitle("MEMORY");
         graphA.getViewport().setMinY(0);
         graphA.getViewport().setMaxY(100);
-
         graphA.getViewport().setYAxisBoundsManual(true);
 
         LineGraphSeries<DataPoint> series4 = new LineGraphSeries<DataPoint>(new DataPoint[]{
@@ -40,7 +53,7 @@ public class Flipcardsp extends AppCompatActivity {
                 new DataPoint(2, 20),
                 new DataPoint(3, 55),
                 new DataPoint(4, 75),
-                new DataPoint(5, 90),
+                new DataPoint(5, 20),
 //                new DataPoint(0, 70),
 //                new DataPoint(0, 80),
 //                new DataPoint(0, 60),
@@ -50,12 +63,57 @@ public class Flipcardsp extends AppCompatActivity {
 
         graphA.addSeries(series4);
         graphA.getViewport().setScalableY(true);
+        graphA.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
         graphA.setBackgroundColor(Color.WHITE);
-        series4.setThickness(5);
+        series4.setThickness(7);
+        series4.setTitle("MEMORY");
         series4.setColor(Color.RED);
         series4.setAnimated(true);
         series4.setDrawDataPoints(true);
         series4.setDataPointsRadius(10);
+
+        pb.setMax(100);
+        final Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(i<=80){
+                    pb.setProgress(i);
+                    textView.setText(i+"%");
+                    i++;
+                    handler.postDelayed(this,10);
+
+
+                }else {
+                    handler.removeCallbacks(this);
+                }
+
+            }
+        },10);
+
+
+        pb1.setMax(100);
+        final Handler handler1=new Handler();
+        handler1.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(j<=80){
+                    pb1.setProgress(j);
+                    textView1.setText(j+"%");
+                    j++;
+                    handler1.postDelayed(this,20);
+
+
+                }else {
+                    handler1.removeCallbacks(this);
+                }
+
+            }
+        },20);
+
+
+
+
 
     }
 }
